276a0d2635d3fe7720402535762b8fc9b2318975
meenamahadikgate6 <meena.mahadik@gate6.com>
2025-04-04 18:08:06 +0530

    storybook installation

---
+++ web-component-v19-main/src/app/status-indicator/status-indicator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-indicator',
+  imports: [],
+  templateUrl: './status-indicator.component.html',
+  styleUrl: './status-indicator.component.scss'
+})
+export class StatusIndicatorComponent {
+
+}
